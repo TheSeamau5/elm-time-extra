86bd60997889b087c38862a8a26a31461ffbaf2b
Hassan Hayat <hassan.hayat7@gmail.com>
2015-04-29 20:42:26 +0100

    Added docs

--- src/Time/Extra.elm
+++ src/Time/Extra.elm
@@ -1,8 +1,16 @@
 module Time.Extra where
+{-| Extra time functions
+
+@docs now
+
+-}
 
 import Task exposing (Task)
+import Time exposing (Time)
 
 import Native.Time.Extra
 
+{-| Task that returns the current time
+-}
 now : Task error Float
 now = Native.Time.Extra.getCurrentTime
